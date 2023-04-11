cdd433ed1e3e7656b77be3a698b5d930f37b9cb6
wendev21 <wendev21@gmail.com>
2023-04-11 18:28:59 +0200

    add terser source cssnano

--- 04FestivalMusica/gulpfile.js
+++ 04FestivalMusica/gulpfile.js
@@ -6,7 +6,7 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
-
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const imagemin = require('gulp-imagemin');
@@ -14,6 +14,9 @@ const webp = require('gulp-webp');
 const cache = require('gulp-cache');
 const avif = require('gulp-avif');
 
+//javascript
+
+const terser = require('gulp-terser-js');
 function css(done)
 {    
     src('src/scss/**/*.scss')//identificar el atchivo de SASS
@@ -55,8 +58,12 @@ function imagenes(done)
     .pipe( dest('build/img'))
     done();
 }
-function javascript(done){
+function javascript(done)
+{
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'));
 
     done();
