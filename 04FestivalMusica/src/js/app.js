5cd14aaf2a61954af6cad477277e742d40da0590
Wendev21 <Wendydev99@gmail.com>
2024-07-16 13:41:49 +0200

    update

--- 04FestivalMusica/src/js/app.js
+++ 04FestivalMusica/src/js/app.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+    navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 })
+
+function navegacionFija() {
+     const header = document.querySelector('.header');
+     const sobreFestival = document.querySelector('.sobre-festival');
+
+     window.addEventListener('scroll', () => {
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed');
+        }else{
+            header.classList.remove('fixed');
+        }
+
+     })
+}
 
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
@@ -28,6 +45,15 @@ function mostrarImagen(index){
     modal.onclick = cerrarModal;
     modal.appendChild(imagen);
 
+    //Boton de cerrar modal
+    const cerrarmodalBtn = document.createElement('BUTTON');
+    cerrarmodalBtn.textContent = 'X';
+    cerrarmodalBtn.classList.add('btn-cerrar');
+    cerrarmodalBtn.onclick = cerrarModal;
+    modal.appendChild(cerrarmodalBtn);
+
+
+
     //agregar al HTML
     const body = document.querySelector('body');
     body.classList.add('overflow-hidden');
@@ -42,4 +68,38 @@ function cerrarModal(){
     setTimeout(() => {
         modal?.remove();
     }, 500);
+}
+function resaltarEnlace(){
+    document.addEventListener('scroll', ()=>{
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+        
+        let actual = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3))
+            {
+                actual = section.id;
+            }
+        });
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active');
+            }
+        });
+    })
+}
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', e =>{
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section =document.querySelector(sectionScroll);
+            section.scrollIntoView({behaivor: 'smooth'})
+        })
+    })
 }
